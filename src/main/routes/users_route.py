daedbe46a7645eb0ffcf521042fd1e2e0a0553b5
Amilcar Cesar <amilcarcesar0@yahoo.com.br>
2026-06-12 15:06:56 -0300

    feat: implementing routes for search and create users with validators

--- src/main/routes/users_route.py
+++ src/main/routes/users_route.py
@@ -1,17 +1,22 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 from src.views.http_types.http_request import HttpRequest
+from src.validators.user_register_validator import UserInput
+
 from src.main.composer.user_finder_composer import user_finder_composer
-
-
+from src.main.composer.user_register_composer import user_register_composer
 
 users_routes = APIRouter(tags=["usuários"])
 
 @users_routes.post("/users")
-async def criar_usuario():
+async def criar_usuario(body: UserInput):
+    http_request = HttpRequest(body=dict(body))
+    user_register = user_register_composer()
 
-    return JSONResponse(content={"message": "Usuário criado com sucesso!"},
-                         status_code=201)
+    http_response = await user_register.handle_register_user(http_request)
+    
+    return JSONResponse(content=http_response.body,
+                         status_code=http_response.status_code)
 
 @users_routes.get("/users/{username}")
 async def buscar_usuarios_por_nome(username: str):
